f0c378b51db0923f2189a2123fe6364c657ad8d2
BNamek <151549184+BNamek@users.noreply.github.com>
2025-12-16 11:38:06 +0100

    Update Carac.java

--- Carac.java
+++ Carac.java
@@ -1,5 +1,27 @@
 class Carac{
-    int caca;
-    int test;
-    ttt
+    String nomCarac;
+    double valeur;
+
+    public Carac(String nc, double val){
+        if (nc.equals("")){
+            this.nomCarac="Inconnu";
+        }
+        else {
+            this.nomCarac=nc;
+        }
+        this.valeur=val;
+    }
+
+    public double getValeur(){
+        return this.valeur;
+    }
+    
+    public boolean etreEgal(String nom){
+        if (nom.equals(this.nomCarac)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
