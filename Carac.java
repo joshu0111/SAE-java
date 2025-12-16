93bb840a2155e52b5933675fb4a428f0feff9013
joshu0111 <130243966+joshu0111@users.noreply.github.com>
2025-12-16 22:52:56 +0100

    Update Carac.java

--- Carac.java
+++ Carac.java
@@ -1,7 +1,12 @@
 class Carac{
+    /* Attributs */
     String nomCarac;
     double valeur;
 
+    /** Constructeurs avec paramatre 
+    * @param nc chaine de caractere qui correspond au nom de la caracteristique 
+    * @param val reel qui correspond a la valeur de la caracteristique 
+    */
     public Carac(String nc, double val){
         if (nc.equals("")){
             this.nomCarac="Inconnu";
@@ -12,10 +17,17 @@ class Carac{
         this.valeur=val;
     }
 
+    /**
+     * Methode getter pour l'attribut valeur 
+     */
     public double getValeur(){
         return this.valeur;
     }
     
+    /**
+     * Methode etreEgal qui renvoie truesi le nom passer en parametre est egal a l'attribut nomCarac
+     * @param nom qui permet 
+     */
     public boolean etreEgal(String nom){
         if (nom.equals(this.nomCarac)){
             return true;
