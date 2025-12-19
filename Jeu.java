21bbc9694ec9f7c7a739844b27e5806ef07d0a04
BNamek <151549184+BNamek@users.noreply.github.com>
2025-12-19 10:23:16 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -41,44 +41,89 @@ class Jeu{
     public void jouerUneManche(){
         Scanner sc=new Scanner(System.in);
         Random rm=new Random();
-
+        
+        // On choisit une carte au hasard dans la pioche
         Carte cartePioche=pioche.piocherHasard();
         if (cartePioche==null){
             return;
         }
+        // On rends visible la carte
         cartePioche.setVisible();
-        System.out.println("Voici la carte pioche par le joueur : " + cartePioche);
+        
+        afficherTitre("CARTE ET CARAC");
+        System.out.println(cartePioche.toString());
+        // On choisit une caracteristique au hasard
         Carac[] caracs = cartePioche.getCaracs();
         int indiceCarac = rm.nextInt(caracs.length);
         String carachasard = caracs[indiceCarac].getNomCarac();
         System.out.println("caracteristique testee :" + carachasard);
-        System.out.println("Voici la main du joueur");
-        for (int i=0; i<main.getNbCartes(); i++) {
-            System.out.println(i + ". " + main.getCarte(i));
-        }
-        System.out.print("Choisissez la carte la plus proche :  ");
+        afficherTitre("");
+        
+        afficherTitre("MAIN DU JOUEUR");
+        System.out.println(main.toString());
+        afficherTitre("");
+        afficherTitre("CHOIX");
+        System.out.println("Quelle est la carte de votre main la plus proche ?");
+        // On demande au joueur de faire un choix entre 0 et main-1
         int choix= sc.nextInt();
+        afficherTitre("");
+        // On trouve l'indice de la vrai meilleure carte
         int bc= main.trouverCarteProche(cartePioche, carachasard);
         Carte carteJoue= main.getCarte(choix);
+        
+        afficherTitre("CARTE JOUEE");
+        System.out.println(carteJoue.toString());
+        afficherTitre("");
+
         if (choix==bc){
-            System.out.println("Vous avez choisit la meilleure carte.");
+            afficherTitre("REPONSE");
+            System.out.println("bien joue, y avait pas mieux.");
+            // On retire la carte de la main du joueur sans lui en rajouter
             main.retirerCarte(choix);
+            afficherTitre("");
         } else{
-            System.out.println("Dommage, il y'avais une carte plus proche.");
+            afficherTitre("REPONSE");
+            System.out.println("eh non, il y avait une carte plus proche:");
             Carte mr=main.getCarte(bc);
             mr.setVisible();
-            System.out.println("Voici la meilleure carte : " + mr);
+            System.out.println(mr.toString());
+            System.out.println("on les retire et on ajoute deux nouvelles cartes");
+            // On retire les deux cartes du joueur puis il en re pioche deux 
             main.retirerCarte(choix);
             main.retirerCarte(bc);
             piocher();
             piocher();
+            afficherTitre("");
         }
     }
+    /**
+     * 
+     */
+    public void afficherTitre(String mot){
+        String res = "";
+        if (mot.isBlank()){
+            for (int i=0; i<80; i++){
+                res += "-";
+            }
+        }
+        else {
+            int moitie = (80-mot.length())/2;
+            for (int i=0; i<moitie;i++){
+                res+= "-"; 
+            }
+            res += mot;
+            for (int i=res.length(); i<80; i++){
+                res+= "-"; 
+            }
+        }
+        System.out.println(res); 
+    }
         
-        /**
-         * Methode qui permet de jouer une partie complete
-         */
+    /**
+    * Methode qui permet de jouer une partie complete
+    */
     public void jouer(){
+        // Tant que le joueur n'a pas la main vide ou que la pioche est vide
         while (main.getNbCartes() > 0 && pioche.getNbCartes() > 0) {
             jouerUneManche();
         }
@@ -88,25 +133,4 @@ class Jeu{
             System.out.println("Defaite, la pioche est vide.");
         }
     }
-
-    
-	(String mot)
-    String res = "";
-      if (mot.isBlank()){
-          for (int i=0; i<80; i++){
-              res += "-";
-          }
-      }
-      else {
-          int moitie = (80-mot.length())/2;
-          for (int i=0; i<moitie;i++){
-              res+= "-"; 
-          }
-          res += mot;
-          for (int i=res.length(); i<80; i++){
-              res+= "-"; 
-          }
-      }
-      System.out.println(res); 
-
 }
